7b566a91a2d43534227431f63e40ab035f2cfe40
Agata <amomot@bio-prodict.nl>
2022-03-25 01:50:17 +0100

    Worked with {0, 0}

--- poly/polynomial_degree.asm
+++ poly/polynomial_degree.asm
@@ -5,11 +5,11 @@ polynomial_degree:
 	mov rbx, rdi
 	mov rcx, rsi
 	mov rax, 0xFFFFFFFF
-	ret
 
 .check_zero_poly:
-	test [rbx], [rbx]
-	jnz .ret_zero_poly
+	mov rdx, [rbx]
+	test rdx, rdx
+	jnz .ret_non_zero_poly
 	lea rbx, [rbx + 4]
 	loop .check_zero_poly
 
