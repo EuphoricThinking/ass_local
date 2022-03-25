9cc42e88371bc602d689b404d328a715d58418c4
Agata <amomot@bio-prodict.nl>
2022-03-25 14:30:19 +0100

    Segfault with {5, 5}

--- poly/polynomial_degree_example.c
+++ poly/polynomial_degree_example.c
@@ -11,16 +11,16 @@ int main() {
 	const int poly1[] = {1, 4, 9, 16, 25, 36, 49, 64, 81};
 //	const int degree1 = 2;
 	size_t size_1 = 9;
-*/
+
 	const int poly2[] = {777};
 //	const int degree2 = 0;
 	size_t size_2 = 1;
-
-/*	const int poly3[] = {5, 5};
+*/
+	const int poly3[] = {5, 5};
 //	const int degree3 = 0;
 	size_t size_3 = 2;
 
-	const int poly4[] = {0};
+/*	const int poly4[] = {0};
 //	const int degree4 = -1;
 	size_t size_4 = 1;
 
@@ -32,7 +32,7 @@ int main() {
 //	const int degree6 = -1;
 	size_t size_6 = 1; */
 
-	printf("%d\n", polynomial_degree(poly2, size_2));
+	printf("%d\n", polynomial_degree(poly3, size_3));
 
 	return 0;
 }
